b4e033bd2cadf7b5e3ac6f6b4aecccba6119105e
Victor Melo <victorsilvamelo13123@gmail.com>
2023-05-16 08:53:21 -0300

    Tarefa 1

---
+++ Jogador_Futebol/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+struct JogadorFutebol
+{
+    public string nome;
+    public int idade;
+    public string clubeAtual;
+    public int numeroCartoesAmarelos;
+    public int numeroCartoesVermelhos;
+
+    public void RegistrarNumeroCartoesAmarelos()
+    {
+        numeroCartoesAmarelos++;
+    }
+
+    public void RegistrarNumeroCartoesVermelhos()
+    {
+        numeroCartoesVermelhos++;
+    }
+
+    public bool VerificarVinculoClube()
+    {
+        return !string.IsNullOrEmpty(clubeAtual);
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Idade: " + idade);
+        Console.WriteLine("Clube atual: " + clubeAtual);
+        Console.WriteLine("Número de cartões amarelos: " + numeroCartoesAmarelos);
+        Console.WriteLine("Número de cartões vermelhos: " + numeroCartoesVermelhos);
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            JogadorFutebol jogador = new JogadorFutebol();
+            jogador.nome = "Luva de Pedreiro";
+            jogador.idade = 19;
+            jogador.clubeAtual = "Clube das Capivaras";
+            jogador.numeroCartoesAmarelos = 2;
+            jogador.numeroCartoesVermelhos = 1;
+
+            jogador.Imprimir(); // Imprime os dados do jogador
+
+            jogador.RegistrarNumeroCartoesAmarelos(); // Incrementa o número de cartões amarelos
+            jogador.RegistrarNumeroCartoesVermelhos(); // Incrementa o número de cartões vermelhos
+
+            bool possuiVinculo = jogador.VerificarVinculoClube(); // Verifica se o jogador possui vínculo com algum clube
+
+            jogador.Imprimir(); // Imprime os dados atualizados do jogador
+        }
+    }
+
+}
